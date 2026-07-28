62db2104f055dffea7e1daa9dad85b55f2d965eb
Sanu Biswas <sanubiswas100274@gmail.com>
2026-07-28 15:03:28 +0530

    chore: add window.Buffer polyfill at entrypoint for browser compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import { Buffer } from 'buffer';
+if (typeof window !== 'undefined') {
+  (window as any).Buffer = Buffer;
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
